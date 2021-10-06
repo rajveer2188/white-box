2919c76c38940b82fcf44345b64aa4090431ce9e
rajveer2188 <92036661+rajveer2188@users.noreply.github.com>
2021-10-06 19:15:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var ball
+function setup() {
+
+  createCanvas(400,400)
+  ball = createSprite(200,200,10,10)
+  ball.shapeColour = "white"
+
+
+}
+
+function draw() 
+{
+  background("black");
+
+if(keyIsDown(RIGHT_ARROW))
+{
+ball.x = ball.x+2
+}
+
+
+if(keyIsDown(LEFT_ARROW))
+{
+ball.x = ball.x-2
+}
+
+
+if(keyIsDown(UP_ARROW))
+{
+ball.y = ball.y-2
+}
+
+
+if(keyIsDown(DOWN_ARROW))
+{
+ball.y = ball.y+2
+}
+
+
+
+
+
+drawSprites()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
